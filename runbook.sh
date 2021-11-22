26fbc9adec802196d81431d016573ff71741a201
Omur Ozbahceliler <omur@rootifera.org>
2021-11-22 11:32:51 +0000

    Update runbook.sh

--- runbook.sh
+++ runbook.sh
@@ -2,4 +2,4 @@
 
 echo "test"
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install apache2 -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install mariadb-server-10.3 -y
